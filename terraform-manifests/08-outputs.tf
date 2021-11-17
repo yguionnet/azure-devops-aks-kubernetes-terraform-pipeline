1d63a80ffbbd9a99c104d55a5c2b5446e78512eb
yguionnet <yannick.guionnet@guiodesv.fr>
2021-11-17 17:26:31 +0100

    fix azure ad group issue

--- terraform-manifests/08-outputs.tf
+++ terraform-manifests/08-outputs.tf
@@ -27,7 +27,7 @@ output "latest_version" {
 
 # Azure AD Group Object Id
 output "azure_ad_group_id" {
-  #value = azuread_group.aks_administrators.id
+  value = azuread_group.aks_administrators.id
 }
 output "azure_ad_group_objectid" {
   value = azuread_group.aks_administrators.object_id
